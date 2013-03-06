6a59363d990ad3252be1cf39ddd9ae259c1888e1
Sina Khanifar <sina.khanifar@gmail.com>
2013-03-06 08:41:50 -0600

    validation

--- preview/js/script.js
+++ preview/js/script.js
@@ -265,3 +265,36 @@ function didSubmitEmail(d) {
                 $('div#legtweets').removeClass('hide');
 
             });
+
+
+// FORM VALIDATION
+
+$(document).ready(function(){
+ 
+ $('#rep-form').validate(
+ {
+  rules: {
+    name: {
+      minlength: 2,
+      required: true
+    },
+    email: {
+      required: true,
+      email: true
+    },
+    "zip-code": {
+      minlength: 5,
+      required: true
+    }
+  },
+  highlight: function(element) {
+    $(element).closest('.control-group').removeClass('success').addClass('error');
+  },
+  success: function(element) {
+    element
+    .text('OK!').addClass('valid')
+    .closest('.control-group').removeClass('error').addClass('success');
+  }
+ });
+}); // end document.ready
+
